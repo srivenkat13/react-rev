dca186090cd2249ed1c831fd1cdc818eb9bf64c7
srivenkat13 <srivenkatsairaj13@gmail.com>
2024-01-07 18:22:00 +0530

    functional Component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const userStatus = false;
-const signedInMsg = " User is Signed In";
-const signedOutMSg = " User  Logged out";
+const userStatus = true;
+const signedInMsg = "User is Signed In";
+const signedOutMSg = "User  Logged out";
 
 const signedInElement = <div>{signedInMsg}</div>;
 const signedOutElement = <div>{signedOutMSg}</div>;
 const userGreeting = userStatus ? signedInElement : signedOutElement;
 
+const GreetUser = function () {
+    return (
+        <>
+        {userGreeting}
+        </>
+    )
+}
+
 const div2 = <div className="text">Hello from react with webpack</div>;
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(userGreeting);
+root.render(<GreetUser />);
