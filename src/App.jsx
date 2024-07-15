172fed2fc9db3de49417a2d0201ee8f9609de949
Venkat <srivenkatsairaj13@gmail.com>
2024-07-15 15:57:47 +0530

    rendering a list, key prop

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
-import React from "react";
-
+import React, { useState } from "react";
 import Button from "./Button.jsx";
+import Text from "./Text.jsx";
+
 const App = () => {
-  const handleClickPar =() => {
-    alert(' clicked from Parent component')
-  }
-  return  <>
-     <Button clickAction= {handleClickPar} data = "Click me"/>
-    
-  </>
+  const handleClickPar = () => {
+    alert(" clicked from Parent component");
+  };
+
+  const addMoreData = (first) => { 
+    setData((prev) =>  [ {id: 'e', text: 'Drona'}, ...prev] )
+   }
+
+  const [data, setData] = useState([
+    { id: "a", text: "Arjuna" },
+    { id: "b", text: "Bheema" },
+    { id: "c", text: "Chanakya" },
+    { id: "d", text: "Duryodhana" },
+  ]);
+  return (
+    <>
+    <h2>Open console to see difference </h2>
+    <h3>List with Index as key</h3>
+      {data.map((item,index) => {
+        return <Text key={index}>{item.text}</Text>;
+      })}
+      <Button clickAction={addMoreData} data="Add more data" />
+    <h3>List with id as key</h3>
+      {data.map((item,index) => {
+        return <Text key={item.id}>{item.text}</Text>;
+      })}
+      <Button clickAction={addMoreData} data="Add more data" />
+    </>
+  );
 };
 
 export { App };
